a0b900db4c5d7971808e46a2bb95008c0b850eef
julian helmholz <julian.helmholz@gmx.de>
2018-06-04 13:54:33 +0200

    sdf

--- Struktogramm Aufgabe 2/Program.cs
+++ Struktogramm Aufgabe 2/Program.cs
@@ -10,6 +10,31 @@ namespace Struktogramm_Aufgabe_2
     {
         static void Main(string[] args)
         {
+            Console.Write("Geben Sie eine ganzzahlige zahl ein: ");
+            int zahl1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Geben Sie eine ganzzahlige zahl ein: ");
+            int zahl2 = Convert.ToInt32(Console.ReadLine());
+
+            while (zahl1 < 10)
+            {
+                zahl2 = zahl1 * 10;
+                Console.WriteLine(zahl1);
+                Console.WriteLine(zahl2);
+                zahl1 = zahl1 + zahl1;
+
+            }
+            Console.ReadLine();
+
+           /*int wert = 1;
+
+            for (int i = 1; i <= Zahl2; i++)
+            {
+                
+                wert = wert * Zahl1;
+               
+            }
+            Console.WriteLine(wert);
+            Console.ReadLine();*/
 
         }
     }
