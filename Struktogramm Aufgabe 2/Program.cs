811830c787e5853b232f8760d2b2388a8c59638c
julian helmholz <julian.helmholz@gmx.de>
2018-06-04 11:17:44 +0200

    Projektdateien hinzufügen.

---
+++ Struktogramm Aufgabe 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struktogramm_Aufgabe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
